7a4d33a62be15c923b384516c4c3c57f7b91f238
Łukasz Niemier <lukasz.niemier@kobil.com>
2020-08-04 19:16:34 +0200

    FT SCP-1185: document functions of the Urepo

--- lib/urepo.ex
+++ lib/urepo.ex
@@ -1,6 +1,12 @@
 defmodule Urepo do
   alias Urepo.Store
 
+  @type tarball() :: iodata()
+
+  @doc """
+  Publish release for given tarball.
+  """
+  @spec publish_release(tarball()) :: {:ok, map()} | {:error, term()}
   def publish_release(tarball) do
     store = store()
 
@@ -13,6 +19,11 @@ defmodule Urepo do
     end
   end
 
+  @doc """
+  Publish documentation for package `name` with `version` using `tarball`.
+  """
+  @spec publish_docs(name :: binary(), version :: binary(), tarball()) ::
+          :ok | {:error, term()}
   def publish_docs(name, version, tarball) do
     store = store()
 
@@ -20,16 +31,26 @@ defmodule Urepo do
          do: :ok
   end
 
+  @doc """
+  Load documentation from the store for given package and version.
+  """
+  @spec get_docs(name :: binary(), version :: binary()) ::
+          {:ok, [{Path.t(), iodata()}]} | {:error, term()}
   def get_docs(name, version) do
     with {:ok, tarball} <- Store.fetch(store(), Path.join(["docs", "#{name}-#{version}.tar"])),
          {:ok, files} <- :hex_tarball.unpack_docs(tarball, :memory),
          do: {:ok, files}
   end
 
+  @doc "Get name for the current repository"
   def name, do: Application.get_env(:urepo, :name, "urepo")
 
+  @doc "Get store configuration for current repository"
   def store, do: Application.fetch_env!(:urepo, :store)
 
+  @doc "Get content of PEM encoded file containing private key"
   def private_key, do: File.read!(Application.get_env(:urepo, :private_key))
+
+  @doc "Get content of PEM encoded file containing public key"
   def public_key, do: File.read!(Application.get_env(:urepo, :public_key))
 end
