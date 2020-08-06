6224d4fd3893e8f0c987477c32770b4d0822425e
Javid Rzayev <rz.cavid@gmail.com>
2020-08-06 15:06:46 +0400

    FT SCP-1185: Prepare image

---
+++ config/prod.exs
@@ -0,0 +1,14 @@
+import Config
+
+config :urepo,
+  token: "secret",
+  public_key: "public.pem",
+  private_key: "private.pem",
+  store: {Urepo.Store.Local, bucket: "repo", path: "repo"}
+
+config :ex_aws, :s3,
+  scheme: "http://",
+  host: "localhost",
+  port: 9000,
+  access_key_id: "minioadmin",
+  secret_access_key: "minioadmin"
