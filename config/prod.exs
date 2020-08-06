89105be59737d332a13092cc1785f367c5e6f748
Łukasz Niemier <lukasz.niemier@kobil.com>
2020-08-06 18:23:51 +0200

    FIX SCP-1185: add Hackney do dependencies for S3

--- config/prod.exs
+++ config/prod.exs
@@ -4,11 +4,8 @@ config :urepo,
   token: "secret",
   public_key: "public.pem",
   private_key: "private.pem",
-  store: {Urepo.Store.Local, bucket: "repo", path: "repo"}
+  store: {Urepo.Store.S3, bucket: "repo"}
 
 config :ex_aws, :s3,
-  scheme: "http://",
-  host: "localhost",
-  port: 9000,
   access_key_id: "minioadmin",
   secret_access_key: "minioadmin"
