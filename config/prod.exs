affbffc0e58885064a3af6ac20179215f14329e1
Łukasz Niemier <lukasz.niemier@kobil.com>
2020-08-06 18:26:01 +0200

    FIX SCP-1185: use local store by default
    This will make testing the application startup easier in CI.

--- config/prod.exs
+++ config/prod.exs
@@ -4,8 +4,4 @@ config :urepo,
   token: "secret",
   public_key: "public.pem",
   private_key: "private.pem",
-  store: {Urepo.Store.S3, bucket: "repo"}
-
-config :ex_aws, :s3,
-  access_key_id: "minioadmin",
-  secret_access_key: "minioadmin"
+  store: {Urepo.Store.Local, path: "/tmp/repo"}
